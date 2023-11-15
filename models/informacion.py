d605ff578728cdd23c308542bcb5e5c09e5740cd
a22samuelfn <samuelfraguas123@gmail.com>
2023-11-15 14:05:47 +0100

    volume automatico

--- models/informacion.py
+++ models/informacion.py
@@ -10,7 +10,13 @@ class informacion(models.Model):
     alto_en_cms = fields.Integer(string="Alto en centímetros")
     longo_en_cms = fields.Integer(string="Longo en centímetros")
     ancho_en_cms = fields.Integer(string="Ancho en centímetros")
+    volume = fields.Float(compute="_volume",stor=True)
     peso = fields.Float(digits=(6,2), default=2.7, string="Peso en KG.s")
     autorizado = fields.Boolean(default=True, string="¿Autorizado?")
     sexo_traducido = fields.Selection([("Hombre", "Home"), ("Mujer", "Muller"), ("Otros", "Outros")], string="Sexo:")
     # ("Hombre", "Home") el primero se guarda en BBDD, el segundo es el que se muestra en Odoo
+
+    @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
+    def _volume(self):
+        for rexistro in self:
+            rexistro.volume = float(rexistro.alto_en_cms) * float(rexistro.longo_en_cms) * float(rexistro.ancho_en_cms)
