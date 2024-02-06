123eb1a3680c94525ab31cf0f91592fbfc5e7946
a22samuelfn <samuelfraguas123@gmail.com>
2024-02-06 14:26:18 +0100

    relaciones many2many

--- models/lineapedido.py
+++ models/lineapedido.py
@@ -10,3 +10,7 @@ class lineapedido(models.Model):
     descripcion = fields.Text(string="Comentario da línea de pedido")
     pedido_id = fields.Many2one('odoo_basico.pedido',
                                 ondelete="cascade", required=True)
+    informacion_ids = fields.Many2many("odoo_basico.informacion",
+                                       string="Rexistro de Información",
+                                       relation="odoo_basico_lineapedido_informacion",
+                                       column1="lineapedido_id", column2="informacion_id")
